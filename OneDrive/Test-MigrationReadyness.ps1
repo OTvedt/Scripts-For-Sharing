bd2a0f6b8690f38f097dcd6b138e7d4115a1bb2c
Olav Tvedt <olav@tvedt.info>
2019-02-18 13:45:47 +0100

    Created Test-MigrationReadyness.ps1
    This is an add-on to make Test-OneDrivePath.ps1 only check against only specified folders (and its subfolders) instead of all folders in the specified path. I have used the SharePoint Migration Tool's input file as the source for folders since this makes me able to combine one input file with the migration itself. But any CSV file with the first column being a path can be used.
    
    All scanned top folders (Users home folder) which contain errors will result in a log file. Top folders with no errors will not get a result file. The process itself is logged in the Test-MigrationReadyness.log. Here you will find info about when the process started and ended for each top folder.

---
+++ OneDrive/Test-MigrationReadyness.ps1
@@ -0,0 +1,27 @@
+$Global:FilePath = "C:\Tools\" #Path to csv file
+$Global:CsvName = "User-Info.csv" # Name of csv file
+$Global:LogfileLocation = "FileScanErrors" # Name of log filelocation
+$Global:Csv = $Global:FilePath + $Global:CsvName
+$Global:Script = "C:\Tools\Test-OneDrivePath.ps1" #Location of the test-ondrivepath.ps script
+$Global:parameters =' -Path $Item -OutFolder $FilePath\$LogfileLocation -ErrorsOnly' #Paramters used with the script remove -ErrorsOnly to include warnings
+$Global:ScriptLine = $script + $parameters
+
+$csvItems = import-csv $Csv -Header c1
+
+   # Write-Output "--------------------------" >> $FilePath\Test-MigrationReadyness.log
+   # Write-Output "Started Test-MigrationReadyness.ps1 Process - $(Get-Date)" >> $FilePath\Test-MigrationReadyness.log
+   # Write-Output "Resultat are located in a subfolder of " >> $FilePath\Test-MigrationReadyness.log
+
+Write-Output "Started checking folder readyness Process - $(Get-Date)" >> $FilePath\Test-MigrationReadyness.log
+
+ForEach ($item in $csvItems.c1)
+{
+
+   Write-Output "Starting with $($item) - $(Get-Date)" >> $FilePath\Test-MigrationReadyness.log
+   Invoke-Expression $ScriptLine
+   Write-Output "Finsihed with $($item) - $(Get-Date)" >> $FilePath\Test-MigrationReadyness.log
+   Write-Output "";"" >> $FilePath\Test-MigrationReadyness.log  
+}
+
+Write-Output "Ended checking folder readyness Process - $(Get-Date)" >> $FilePath\Test-MigrationReadyness.log
+ Write-Output "";"" >> $FilePath\Test-MigrationReadyness.log
