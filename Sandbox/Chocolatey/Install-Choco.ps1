bf8014a9fd0aded650e1256f5ae94d49de5a6cff
Olav Tvedt <olav@tvedt.info>
2019-11-11 11:32:52 +0100

    Create Install-Choco.ps1
    Install Chocolatey and tools

---
+++ Sandbox/Chocolatey/Install-Choco.ps1
@@ -0,0 +1,1 @@
+Set-ExecutionPolicy Bypass ` -Scope Process ` -Force; ` iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) ; ` choco install googlechrome firefox notepadplusplus azure-cli powershell-core vscode   git -y
