0b1ea10128f0d058174f6f2de87dca101ab16b79
Olav Tvedt <olav@tvedt.info>
2019-11-11 11:34:34 +0100

    Create Prep.cmd
    - Change wallpaper
    - Starts Install-Choco.ps1 script
    - Refresh wallpaper (twice, since it sometimes is slow)
    Pause is just to verify

---
+++ Sandbox/Chocolatey/Prep.cmd
@@ -0,0 +1,7 @@
+Powershell Set-ExecutionPolicy unrestricted
+Powershell -file C:\Users\WDAGUtilityAccount\Desktop\Chocolatey\Set-WallPaper.ps1
+Timeout 10
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
+Powershell C:\Users\WDAGUtilityAccount\Desktop\Chocolatey\Install-Choco.ps1"
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
+Pause
