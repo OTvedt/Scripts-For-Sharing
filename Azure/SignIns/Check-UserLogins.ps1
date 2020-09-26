ed413de8a8c4f05e371015c72bfba58deb0d20d8
Olav Tvedt <olav@tvedt.info>
2020-09-26 14:31:17 +0200

    Create Check-UserLogins.ps1
    Simple script to search for the last 3 times a user logged in to Azure AD.
    !! Must only use first name or first letter(s) of the first name to make it work

---
+++ Azure/SignIns/Check-UserLogins.ps1
@@ -0,0 +1,16 @@
+Connect-AzureAD
+$User = Read-Host "Enter first name (or start of username) of user account"
+$Users = Get-AzureADUser -SearchString $User
+ForEach ($U in $Users) {
+   If ($U.UserType -eq "Member") {
+      $UserLastLogonDate = $Null
+      Try {
+         $UserObjectId = $U.ObjectId
+         $UserLastLogonDate = (Get-AzureADAuditSignInLogs -Top 3  -Filter "userid eq '$UserObjectId' and status/errorCode eq 0").CreatedDateTime }
+      Catch {
+         Write-Host "Can't read Azure Active Directory Sign in Logs"`n }
+      If ($UserLastLogonDate -ne $Null) {
+         $LastSignInDate = Get-Date($UserLastLogonDate); $Days = New-TimeSpan($LastSignInDate)
+         Write-Host "User" $U.DisplayName "last signed in on" $LastSignInDate "or" $Days.Days "days ago"`n }
+      Else { Write-Host "No Azure Active Directory sign-in data available for" $U.DisplayName "(" $U.Mail ")"`n }
+     }}
