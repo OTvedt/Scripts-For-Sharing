7ad63c0b495fc0116e437c01ecf3b29c5ff7dc7f
Olav Tvedt <olav@tvedt.info>
2020-11-12 14:30:36 +0100

    Minor changes

--- Azure/SignIns/Check-UserLogins.ps1
+++ Azure/SignIns/Check-UserLogins.ps1
@@ -6,11 +6,15 @@ ForEach ($U in $Users) {
       $UserLastLogonDate = $Null
       Try {
          $UserObjectId = $U.ObjectId
-         $UserLastLogonDate = (Get-AzureADAuditSignInLogs -Top 1  -Filter "userid eq '$UserObjectId' and status/errorCode eq 0").CreatedDateTime }
+         $UserLastLogonDate = (Get-AzureADAuditSignInLogs -Top 1  -Filter "userid eq '$UserObjectId' and status/errorCode eq 0").CreatedDateTime 
+      }
       Catch {
-         Write-Host "Can't read Azure Active Directory Sign in Logs"`n }
-      If ($UserLastLogonDate -ne $Null) {
+         Write-Host "Can't read Azure Active Directory Sign in Logs"`n 
+      }
+      If ($Null -ne $UserLastLogonDate) {
          $LastSignInDate = Get-Date($UserLastLogonDate); $Days = New-TimeSpan($LastSignInDate)
-         Write-Host "User" $U.DisplayName "last signed in on" $LastSignInDate "or" $Days.Days "days ago"`n }
+         Write-Host "User" $U.DisplayName "last signed in on" $LastSignInDate "or" $Days.Days "days ago"`n 
+      }
       Else { Write-Host "No Azure Active Directory sign-in data available for" $U.DisplayName "(" $U.Mail ")"`n }
-     }}
+   }
+}
