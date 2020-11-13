15a3763ac6fe355e6ccb7574684199213e612fee
Olav Tvedt <olav@tvedt.info>
2020-11-13 13:26:50 +0100

    Minor updates

--- Azure/Network/Get-NetworkWatcherEnabledInfo.ps1
+++ Azure/Network/Get-NetworkWatcherEnabledInfo.ps1
@@ -5,7 +5,7 @@ Get-AzContext
 $AllSubs = get-AzSubscription | Where-Object { $_.Name -NotMatch 'Visual Studio' -and $_.Name -NotMatch 'Prøv gratis' -and $_.Name -NotMatch 'Tilgang til Azure Active Directory' -and $_.Name -NotMatch 'HaraldVS' }
 $CollectionAll = @(
   Foreach ($Sub in $AllSubs) {
-    $AzSub = Select-AzSubscription -Subscription $Sub.Id # -Tenant xxxxxxxxxxxxxxx
+    $AzSub = Select-AzSubscription -Subscription $Sub.Id -ErrorAction SilentlyContinue -WarningAction SilentlyContinue # -Tenant xxxxxxxxxxxxxxx
     $AzNw = Get-AZNetworkWatcher -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
     foreach ($Watcher in $AzNw) {
       [PSCustomObject]@{
