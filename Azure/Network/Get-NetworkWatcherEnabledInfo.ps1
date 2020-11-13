6044dda36602ceafbf465a98ca8f6d82626fb19a
Olav Tvedt <olav@tvedt.info>
2020-11-13 13:15:35 +0100

    Created Get-NetworkWatcherEnabledInfo.ps1

---
+++ Azure/Network/Get-NetworkWatcherEnabledInfo.ps1
@@ -0,0 +1,20 @@
+﻿# Install-Module Az.Network -Scope CurrentUser -Force
+Connect-AzAccount # -Tenant xxxxxxx 
+Get-AzTenant
+Get-AzContext
+$AllSubs = get-AzSubscription | Where-Object { $_.Name -NotMatch 'Visual Studio' -and $_.Name -NotMatch 'Prøv gratis' -and $_.Name -NotMatch 'Tilgang til Azure Active Directory' -and $_.Name -NotMatch 'HaraldVS' }
+$CollectionAll = @(
+  Foreach ($Sub in $AllSubs) {
+    $AzSub = Select-AzSubscription -Subscription $Sub.Id # -Tenant xxxxxxxxxxxxxxx
+    $AzNw = Get-AZNetworkWatcher -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    foreach ($Watcher in $AzNw) {
+      [PSCustomObject]@{
+        Subscription           = $AzSub.Name;
+        SubscriptionId         = $AzSub.Subscription;
+        NetworkWatcher         = $Watcher.Name
+        NetworkWatcherLocation = $Watcher.Location
+      }
+    }
+  }
+)
+$CollectionAll | Select-Object -Property Subscription, NetworkWatcherLocation | Sort-Object -Property Subscription, NetworkWatcherLocation
