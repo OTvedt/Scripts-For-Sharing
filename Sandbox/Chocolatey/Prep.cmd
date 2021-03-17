17ffd0c1a63a2104e739cb722fb9c23ed1a17796
Olav Tvedt <olav@tvedt.info>
2021-03-17 09:46:22 +0100

    Simplified, removed backgroud picture feature

--- Sandbox/Chocolatey/Prep.cmd
+++ Sandbox/Chocolatey/Prep.cmd
@@ -1,7 +1,3 @@
 Powershell Set-ExecutionPolicy unrestricted
-Powershell -file C:\Users\WDAGUtilityAccount\Desktop\Chocolatey\Set-WallPaper.ps1
-Timeout 10
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
-Powershell C:\Users\WDAGUtilityAccount\Desktop\Chocolatey\Install-Choco.ps1"
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
+Powershell C:\Users\WDAGUtilityAccount\Desktop\SandBox\Chocolatey\Install-Choco.ps1"
 Pause
