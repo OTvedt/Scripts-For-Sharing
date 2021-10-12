24266b9a4fdec822ef74b3d9b511560272db1fa6
Olav-Tvedt <55584819+Olav-Tvedt@users.noreply.github.com>
2021-10-12 13:36:32 +0200

    Created VSCodeInstall.cmd
    File used to install VSCode in Sandbox

---
+++ Sandbox/Visual Studio Code/VSCodeInstall.cmd
@@ -0,0 +1,6 @@
+Timeout 5
+REM Download Visual Studio Code
+curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\users\WDAGUtilityAccount\Desktop\vscode.exe
+
+REM Install and run Visual Studio Code
+C:\users\WDAGUtilityAccount\Desktop\vscode.exe /verysilent /suppressmsgboxes
