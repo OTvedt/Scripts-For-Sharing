9d4e3766bbf1a2426bbe5e5469af81550595b604
Olav Tvedt <olav@tvedt.info>
2021-11-29 10:18:39 +0100

    Update Set-AzureAppOwner.ps1

--- Azure/Apps/Set-AzureAppOwner.ps1
+++ Azure/Apps/Set-AzureAppOwner.ps1
@@ -1,4 +1,4 @@
-#https://docs.microsoft.com/en-us/powershell/module/azuread/add-azureadapplicationowner?view=azureadps-2.0
+#https://docs.microsoft.com/powershell/module/azuread/add-azureadapplicationowner?WT.mc_id=AZ-MVP-4020472
 Connect-AzureAD
 $Owner = Get-AzureADUser -ObjectId Dummy@domain.com
 $FilePath = "C:\Temp\" #Path to csv file
@@ -7,7 +7,6 @@ $Csv = $FilePath + $CsvName
 $Applist = Get-Content $Csv
 
 ForEach ($App in $Applist) {
-    
     $AppName = (Get-AzureADApplication -Filter "DisplayName eq '$App'")
     Add-AzureADApplicationOwner -ObjectId $AppName.ObjectId -RefObjectId $Owner.ObjectId
 }
