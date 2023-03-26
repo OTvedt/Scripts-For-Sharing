da880d57f2a9de3bf904fa60da9b65d08a58d8f4
Olav Tvedt <olav@tvedt.info>
2023-03-26 17:58:57 +0200

    Create RolesUsedInSubs.ps1

---
+++ Azure/PIM/Azure-resources/Document/RolesUsedInSubs.ps1
@@ -0,0 +1,14 @@
+# This will crawl throug all Azure subscriptions to find the Roles that are in use and document it into a csv file
+$me = whoami -upn
+Connect-AzureAD -AccountId $me 
+$Subs = Get-AzSubscription | Where-Object { $_.Name -NotMatch 'Visual Studio' -and $_.Name -NotMatch 'Free' }
+
+ForEach ($Sub in $subs) {
+
+    Set-AzContext -SubscriptionName $sub.Name
+
+    $roles = Get-AzRoleAssignment | Select RoleDefinitionName,RoleDefinitionId  | Sort-Object * | select * -Unique 
+        
+    $roles | Export-Csv -Encoding UTF8 -Path c:\Temp\PIM\$($sub.Name)-RolesInUse-$((Get-Date).ToString('dd.MM.yyyy-hh-mm')).csv -Delimiter ';' -NoTypeInformation
+
+}
