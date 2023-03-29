897facce1fde275ccd39049c2e3ddaa75c7721b7
Olav Tvedt <olav@tvedt.info>
2023-03-29 23:45:14 +0200

    Fixed sort-object

--- Azure/PIM/Azure-resources/Document/RolesUsedInSubs.ps1
+++ Azure/PIM/Azure-resources/Document/RolesUsedInSubs.ps1
@@ -6,7 +6,7 @@ ForEach ($Sub in $subs) {
 
     Set-AzContext -SubscriptionName $sub.Name
 
-    $roles = Get-AzRoleAssignment | Select RoleDefinitionName,RoleDefinitionId  | Sort-Object * | select * -Unique 
+    $roles = Get-AzRoleAssignment | Select RoleDefinitionName,RoleDefinitionId | select * -Unique | Sort-Object -Property RoleDefinitionName
         
     $roles | Export-Csv -Encoding UTF8 -Path c:\Temp\PIM\$($sub.Name)-RolesInUse-$((Get-Date).ToString('dd.MM.yyyy-hh-mm')).csv -Delimiter ';' -NoTypeInformation
 
